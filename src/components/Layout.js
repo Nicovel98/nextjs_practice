66b83f8895a59f54c705f114807497e7a9b7cecd
Nicovel <nicovel98@gmail.com>
2024-07-29 07:03:18 -0500

    Se preparo para poner el footer

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import Navbar from './Navbar'
+import NProgress from "nprogress"; // Importa NProgress para la barra de progreso
+import nProgress from "nprogress"; // Importa nProgress, parece un error de duplicación
 
 const Layout = ({ children }) => {
   return (
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,11 +3,18 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* Nprogress css */}
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.css"
+        />
+      </Head>
+
       <body>
         <Main />
         <NextScript />
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossOrigin="anonymous"></script>
       </body>
     </Html>
   );
